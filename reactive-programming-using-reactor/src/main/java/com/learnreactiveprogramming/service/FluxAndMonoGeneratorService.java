30f16497823ea1b2f38d83a0680c30c3e794547e
Mathias Schulze <mathias-schulze@web.de>
2023-12-17 12:54:54 +0100

    lecture 13+14 create Flux + Mono

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -1,4 +1,23 @@
 package com.learnreactiveprogramming.service;
 
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
 public class FluxAndMonoGeneratorService {
+
+    public Flux<String> namesFlux() {
+        return Flux.fromIterable(List.of("Anton", "Bertram", "Claudia"));
+    }
+
+    public Mono<String> nameMono() {
+        return Mono.just("Daniel");
+    }
+    public static void main(String[] args) {
+
+        FluxAndMonoGeneratorService service = new FluxAndMonoGeneratorService();
+        service.namesFlux().subscribe(name -> System.out.println("Flux name is: " + name));
+        service.nameMono().subscribe(name -> System.out.println("Mono name is: " + name));
+    }
 }
