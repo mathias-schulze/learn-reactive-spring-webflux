e90584a3b56e63e585ad16a11623c21851d6d110
Mathias Schulze <mathias-schulze@web.de>
2023-12-19 20:34:18 +0100

    lecture 16 test Flux

--- reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
+++ reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
@@ -1,7 +1,23 @@
 package com.learnreactiveprogramming.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
 
 class FluxAndMonoGeneratorServiceTest {
 
+    FluxAndMonoGeneratorService service = new FluxAndMonoGeneratorService();
+
+    @Test
+    void namesFlux() {
+
+        Flux<String> namesFlux = service.namesFlux();
+
+        StepVerifier.create(namesFlux)
+                //.expectNext("Anton", "Bertram", "Claudia")
+                //.expectNextCount(3)
+                .expectNext("Anton")
+                .expectNextCount(2)
+                .verifyComplete();
+    }
 }
