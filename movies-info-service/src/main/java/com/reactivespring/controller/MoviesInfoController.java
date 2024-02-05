50da868f7a18eb48c3b46494432eca4b17f93a0e
Mathias Schulze <mathias-schulze@web.de>
2024-02-05 20:11:33 +0100

    lecture 60 get all movies info test

--- movies-info-service/src/main/java/com/reactivespring/controller/MoviesInfoController.java
+++ movies-info-service/src/main/java/com/reactivespring/controller/MoviesInfoController.java
@@ -2,21 +2,48 @@ package com.reactivespring.controller;
 
 import com.reactivespring.domain.MovieInfo;
 import com.reactivespring.service.MovieInfoService;
-import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/v1")
-@AllArgsConstructor
 public class MoviesInfoController {
 
-    private MovieInfoService movieInfoService;
+    private MovieInfoService moviesInfoService;
+
+    public MoviesInfoController(MovieInfoService moviesInfoService) {
+        this.moviesInfoService = moviesInfoService;
+    }
+
+    @GetMapping("/movieinfos")
+    public Flux<MovieInfo> getAllMovieInfos(){
+        return moviesInfoService.getAllMovieInfos().log();
+    }
+
+    @GetMapping("/movieinfos/{id}")
+    public Mono<MovieInfo> getMovieInfoById(@PathVariable String id){
+        return moviesInfoService.getMovieInfoById(id);
+    }
+
     @PostMapping("/movieinfos")
     @ResponseStatus(HttpStatus.CREATED)
-    public Mono<MovieInfo> addMovieInfo(@RequestBody MovieInfo movieInfo) {
+    public Mono<MovieInfo> addMovieInfo(@RequestBody MovieInfo movieInfo){
+        return moviesInfoService.addMovieInfo(movieInfo).log();
 
-        return movieInfoService.addMovieInfo(movieInfo);
+    }
+
+    @PutMapping("/movieinfos/{id}")
+    public Mono<MovieInfo> updateMovieInfo(@RequestBody MovieInfo updatedMovieInfo, @PathVariable String id){
+        return moviesInfoService.updateMovieInfo(updatedMovieInfo, id);
+
+    }
+
+    @DeleteMapping("/movieinfos/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public Mono<Void> deleteMovieInfo(@PathVariable String id){
+        return moviesInfoService.deleteMovieInfo(id);
+
     }
 }
---
+++ movies-info-service/src/test/java/com/reactivespring/controller/MoviesInfoControllerUnitTest.java
@@ -0,0 +1,51 @@
+package com.reactivespring.controller;
+
+import com.reactivespring.domain.MovieInfo;
+import com.reactivespring.service.MovieInfoService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+
+@WebFluxTest(controllers = MoviesInfoController.class)
+@AutoConfigureWebTestClient
+public class MoviesInfoControllerUnitTest {
+
+    static String MOVIES_INFO_URL = "/v1/movieinfos";
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @MockBean
+    private MovieInfoService movieInfoServiceMock;
+
+    @Test
+    void getAllMoviesInfo() {
+
+        var movieinfos = List.of(new MovieInfo(null, "Batman Begins",
+                        2005, List.of("Christian Bale", "Michael Cane"), LocalDate.parse("2005-06-15")),
+                new MovieInfo(null, "The Dark Knight",
+                        2008, List.of("Christian Bale", "HeathLedger"), LocalDate.parse("2008-07-18")),
+                new MovieInfo("abc", "Dark Knight Rises",
+                        2012, List.of("Christian Bale", "Tom Hardy"), LocalDate.parse("2012-07-20")));
+
+        when(movieInfoServiceMock.getAllMovieInfos()).thenReturn(Flux.fromIterable(movieinfos));
+
+        webTestClient
+                .get()
+                .uri(MOVIES_INFO_URL)
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful()
+                .expectBodyList(MovieInfo.class)
+                .hasSize(3);
+    }
+}
