934d267731d0ef499ee3062126c16103ad4bb086
Mathias Schulze <mathias-schulze@web.de>
2024-02-06 21:11:01 +0100

    lecture 75+76 post review + test

--- movies-review-service/src/test/java/intg/com/reactivespring/routes/ReviewsIntgTest.java
+++ movies-review-service/src/test/java/intg/com/reactivespring/routes/ReviewsIntgTest.java
@@ -1,4 +1,63 @@
 package com.reactivespring.routes;
 
+import com.reactivespring.domain.Review;
+import com.reactivespring.repository.ReviewReactiveRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.util.List;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+@AutoConfigureWebTestClient
 public class ReviewsIntgTest {
+
+    static String REVIEWS_URL = "/v1/reviews";
+
+    @Autowired
+    WebTestClient webTestClient;
+
+    @Autowired
+    ReviewReactiveRepository repository;
+
+    @BeforeEach
+    void setUp() {
+        var reviewsList = List.of(
+                new Review(null, 1L, "Awesome Movie", 9.0),
+                new Review(null, 1L, "Awesome Movie1", 9.0),
+                new Review(null, 2L, "Excellent Movie", 8.0));
+        repository.saveAll(reviewsList)
+                .blockLast();
+    }
+
+    @AfterEach
+    void tearDown() {
+        repository.deleteAll().block();
+    }
+
+    @Test
+    void addReview() {
+
+        Review review = new Review(null, 1L, "Awesome Movie", 9.0);
+
+        webTestClient
+                .post()
+                .uri(REVIEWS_URL)
+                .bodyValue(review)
+                .exchange()
+                .expectStatus()
+                .isCreated()
+                .expectBody(Review.class)
+                .consumeWith(reviewEntityExchangeResult -> {
+                    var savedReview = reviewEntityExchangeResult.getResponseBody();
+                    assert savedReview != null;
+                    assert savedReview.getReviewId() != null;
+                });
+    }
 }
